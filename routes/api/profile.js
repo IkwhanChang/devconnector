4987746eb30547e4a5f5fe1f36883cce5dcf949d
Matthew <matthew@urhy.me>
2018-11-29 13:53:13 -0800

    t

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,46 @@ router.get('/', passport.authenticate('jwt', {
     .catch(err => res.status(404).json(err));
 });
 
+// @route   POST api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+  Profile.findOne({
+      user: req.params.user_id
+    })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.profile = 'There is no profile for this user';
+        return req.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+})
+
+// @route   POST api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+  Profile.findOne({
+      handle: req.params.handle
+    })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.profile = 'There is no profile for this user';
+        return req.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+})
+
 // @route   POST api/profile
 // @desc    Create user profile
 // @access  Public
